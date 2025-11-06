9d4e5935ed615feb220088c931c143ebc89c161d
EuzebioZ <e4023784z@gmail.com>
2025-11-05 23:12:38 -0300

    refacter: refatora o codigo adicionando a dependencia LOMBOK

--- src/main/java/br/com/EuzebioZ/cinema/Sessao/SessaoModel.java
+++ src/main/java/br/com/EuzebioZ/cinema/Sessao/SessaoModel.java
@@ -2,22 +2,23 @@ package br.com.EuzebioZ.cinema.Sessao;
 
 import br.com.EuzebioZ.cinema.Telespectador.TelespectadorModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.util.ArrayList;
 
 @Table(name = "tb_sessao")
 @Entity
+@AllArgsConstructor
+@Data
 public class SessaoModel{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
+    @Column(unique=true)
     private long codigo;
 
     @OneToMany(mappedBy = "sessao")
     private ArrayList<TelespectadorModel> telespectadores;
-
-    public SessaoModel() {
-        this.telespectadores = new ArrayList<TelespectadorModel>();
-    }
 
     @Override
     public String toString(){
--- src/main/java/br/com/EuzebioZ/cinema/Telespectador/TelespectadorModel.java
+++ src/main/java/br/com/EuzebioZ/cinema/Telespectador/TelespectadorModel.java
@@ -2,50 +2,32 @@ package br.com.EuzebioZ.cinema.Telespectador;
 
 import br.com.EuzebioZ.cinema.Sessao.SessaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
 
 @Entity
 @Table(name = "tb_telespectador")
+@AllArgsConstructor
+@Data
 public class TelespectadorModel{
     private String nome;
+    @Column(unique=true)
     private String cpf;
+
+    @Column(unique = true)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long codigo;
+        private long codigo;
+
     @ManyToOne
     @JoinColumn(name="sessao_id")
-    private SessaoModel sessao;
-
-    public TelespectadorModel() {}
-
-    public TelespectadorModel(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
-    }
+        private SessaoModel sessao;
 
     @Override
     public String toString(){
         return "Codiogo: "+this.codigo+
         "Nome: "+this.nome+
         "CPF: "+ this.cpf;
-    }
-
-    public long getCodigo() {
-        return codigo;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 }
